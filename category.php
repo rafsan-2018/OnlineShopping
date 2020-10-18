457bd3e1130bd676d14fecf31bc8bf2570ae9d30
sharif fateh shahriar <68992668+rafsan-2018@users.noreply.github.com>
2020-10-18 19:31:52 +0600

    Update category.php

--- category.php
+++ category.php
@@ -21,7 +21,7 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 	}
 	
 }
-// COde for Wishlist
+
 if(isset($_GET['pid']) && $_GET['action']=="wishlist" ){
 	if(strlen($_SESSION['login'])==0)
     {   
@@ -36,3 +36,57 @@ header('location:my-wishlist.php');
 }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+	    <meta name="robots" content="all">
+
+	    <title>Product Category</title>
+
+	   
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    
+	   
+	    <link rel="stylesheet" href="assets/css/main.css">
+	    <link rel="stylesheet" href="assets/css/green.css">
+	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/css/owl.transitions.css">
+		
+		<link href="assets/css/lightbox.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<link rel="stylesheet" href="assets/css/rateit.css">
+		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+
+		
+		<link rel="stylesheet" href="assets/css/config.css">
+
+		<link href="assets/css/green.css" rel="alternate stylesheet" title="Green color">
+		<link href="assets/css/blue.css" rel="alternate stylesheet" title="Blue color">
+		<link href="assets/css/red.css" rel="alternate stylesheet" title="Red color">
+		<link href="assets/css/orange.css" rel="alternate stylesheet" title="Orange color">
+		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
+		
+
+		
+		
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+		
+		
+		<link rel="shortcut icon" href="assets/images/favicon.ico">
+
+		
+
+	</head>
+    <body class="cnt-home">
+	
+<header class="header-style-1">
