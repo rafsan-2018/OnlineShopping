9577a256441bec182ec92aab77da16cb17f5da18
sharif fateh shahriar <68992668+rafsan-2018@users.noreply.github.com>
2020-10-18 19:29:47 +0600

    session start

---
+++ category.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+$cid=intval($_GET['cid']);
+if(isset($_GET['action']) && $_GET['action']=="add"){
+	$id=intval($_GET['id']);
+	if(isset($_SESSION['cart'][$id])){
+		$_SESSION['cart'][$id]['quantity']++;
+	}else{
+		$sql_p="SELECT * FROM products WHERE id={$id}";
+		$query_p=mysqli_query($con,$sql_p);
+		if(mysqli_num_rows($query_p)!=0){
+			$row_p=mysqli_fetch_array($query_p);
+			$_SESSION['cart'][$row_p['id']]=array("quantity" => 1, "price" => $row_p['productPrice']);
+				echo "<script>alert('Product has been added to the cart')</script>";
+		echo "<script type='text/javascript'> document.location ='my-cart.php'; </script>";
+		}else{
+			$message="Product ID is invalid";
+		}
+	}
+	
+}
+// COde for Wishlist
+if(isset($_GET['pid']) && $_GET['action']=="wishlist" ){
+	if(strlen($_SESSION['login'])==0)
+    {   
+header('location:login.php');
+}
+else
+{
+mysqli_query($con,"insert into wishlist(userId,productId) values('".$_SESSION['id']."','".$_GET['pid']."')");
+echo "<script>alert('Product aaded in wishlist');</script>";
+header('location:my-wishlist.php');
+
+}
+}
+?>
