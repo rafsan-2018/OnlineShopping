c2b2b2e68a6c56c622ea9afb46a1467883539572
sharif fateh shahriar <68992668+rafsan-2018@users.noreply.github.com>
2020-11-13 22:04:28 +0600

    roduct info price etc

--- category.php
+++ category.php
@@ -173,3 +173,89 @@ while($row=mysqli_fetch_array($sql))
 
 					       <?php $sql=mysqli_query($con,"select categoryName  from category where id='$cid'");
 
+while($row=mysqli_fetch_array($sql))
+{
+    ?>
+
+					<div class="excerpt hidden-sm hidden-md">
+						<?php echo htmlentities($row['categoryName']);?>
+					</div>
+			<?php } ?>
+			
+				</div>
+			</div>
+		</div>
+</div>
+
+				<div class="search-result-container">
+					<div id="myTabContent" class="tab-content">
+						<div class="tab-pane active " id="grid-container">
+							<div class="category-product  inner-top-vs">
+								<div class="row">									
+			<?php
+$ret=mysqli_query($con,"select * from products where category='$cid'");
+$num=mysqli_num_rows($ret);
+if($num>0)
+{
+while ($row=mysqli_fetch_array($ret)) 
+{?>							
+		<div class="col-sm-6 col-md-4 wow fadeInUp">
+			<div class="products">				
+	<div class="product">		
+		<div class="product-image">
+			<div class="image">
+				<a href="product-details.php?pid=<?php echo htmlentities($row['id']);?>"><img  src="assets/images/blank.gif" data-echo="admin/productimages/<?php echo htmlentities($row['id']);?>/<?php echo htmlentities($row['productImage1']);?>" alt="" width="200" height="300"></a>
+			</div>		                      		   
+		</div>
+			
+		
+		<div class="product-info text-left">
+			<h3 class="name"><a href="product-details.php?pid=<?php echo htmlentities($row['id']);?>"><?php echo htmlentities($row['productName']);?></a></h3>
+			<div class="rating rateit-small"></div>
+			<div class="description"></div>
+
+			<div class="product-price">	
+				<span class="price">
+					TK. <?php echo htmlentities($row['productPrice']);?>			</span>
+										     <span class="price-before-discount">TK. <?php echo htmlentities($row['productPriceBeforeDiscount']);?></span>
+									
+			</div>
+			
+		</div>
+					<div class="cart clearfix animate-effect">
+				<div class="action">
+					<ul class="list-unstyled">
+						<li class="add-cart-button btn-group">
+						
+								<?php if($row['productAvailability']=='In Stock'){?>
+										<button class="btn btn-primary icon" data-toggle="dropdown" type="button">
+								<i class="fa fa-shopping-cart"></i>													
+							</button>
+							<a href="category.php?page=product&action=add&id=<?php echo $row['id']; ?>">
+							<button class="btn btn-primary" type="button">Add to cart</button></a>
+								<?php } else {?>
+							<div class="action" style="color:red">Out of Stock</div>
+					<?php } ?>
+													
+						</li>
+	                   
+		                <li class="lnk wishlist">
+							<a class="add-to-cart" href="category.php?pid=<?php echo htmlentities($row['id'])?>&&action=wishlist" title="Wishlist">
+								 <i class="icon fa fa-heart"></i>
+							</a>
+						</li>
+
+						
+					</ul>
+				</div>
+			</div>
+			</div>
+			</div>
+		</div>
+	  <?php } } else {?>
+	
+		<div class="col-sm-6 col-md-4 wow fadeInUp"> <h3>No Product Found</h3>
+		</div>
+		
+<?php } ?>	
+	
