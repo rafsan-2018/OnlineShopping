296ffb0dddb4ddc6d89762e159b556c6c7a3ab2b
sharif fateh shahriar <68992668+rafsan-2018@users.noreply.github.com>
2020-11-14 22:46:29 +0600

    Update bill-ship-addresses.php

--- bill-ship-addresses.php
+++ bill-ship-addresses.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
-//error_reporting(0);
+
 include('includes/config.php');
 if(strlen($_SESSION['login'])==0)
     {   
 header('location:.php');
 }
 else{
-	// code for billing address updation
+	
 	if(isset($_POST['update']))
 	{
 		$baddress=$_POST['billingaddress'];
@@ -22,7 +22,7 @@ echo "<script>alert('Billing Address has been updated');</script>";
 	}
 
 
-// code for Shipping address updation
+
 	if(isset($_POST['shipupdate']))
 	{
 		$saddress=$_POST['shippingaddress'];
@@ -40,7 +40,7 @@ echo "<script>alert('Shipping Address has been updated');</script>";
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<!-- Meta -->
+		
 		<meta charset="utf-8">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
@@ -51,21 +51,17 @@ echo "<script>alert('Shipping Address has been updated');</script>";
 
 	    <title>My Account</title>
 
-	    <!-- Bootstrap Core CSS -->
+	  
 	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	    
-	    <!-- Customizable CSS -->
 	    <link rel="stylesheet" href="assets/css/main.css">
 	    <link rel="stylesheet" href="assets/css/green.css">
 	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
 		<link rel="stylesheet" href="assets/css/owl.transitions.css">
-		<!--<link rel="stylesheet" href="assets/css/owl.theme.css">-->
 		<link href="assets/css/lightbox.css" rel="stylesheet">
 		<link rel="stylesheet" href="assets/css/animate.min.css">
 		<link rel="stylesheet" href="assets/css/rateit.css">
 		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
-
-		<!-- Demo Purpose Only. Should be removed in production -->
 		<link rel="stylesheet" href="assets/css/config.css">
 
 		<link href="assets/css/green.css" rel="alternate stylesheet" title="Green color">
@@ -81,16 +77,10 @@ echo "<script>alert('Shipping Address has been updated');</script>";
     <body class="cnt-home">
 <header class="header-style-1">
 
-	<!-- ============================================== TOP MENU ============================================== -->
 <?php include('includes/top-header.php');?>
-<!-- ============================================== TOP MENU : END ============================================== -->
 <?php include('includes/main-header.php');?>
-	<!-- ============================================== NAVBAR ============================================== -->
 <?php include('includes/menu-bar.php');?>
-<!-- ============================================== NAVBAR : END ============================================== -->
-
 </header>
-<!-- ============================================== HEADER : END ============================================== -->
 <div class="breadcrumb">
 	<div class="container">
 		<div class="breadcrumb-inner">
@@ -98,9 +88,9 @@ echo "<script>alert('Shipping Address has been updated');</script>";
 				<li><a href="#">Home</a></li>
 				<li class='active'>Checkout</li>
 			</ul>
-		</div><!-- /.breadcrumb-inner -->
-	</div><!-- /.container -->
-</div><!-- /.breadcrumb -->
+		</div>
+	</div>
+</div>
 
 <div class="body-content outer-top-bd">
 	<div class="container">
@@ -108,10 +98,10 @@ echo "<script>alert('Shipping Address has been updated');</script>";
 			<div class="row">
 				<div class="col-md-8">
 					<div class="panel-group checkout-steps" id="accordion">
-						<!-- checkout-step-01  -->
+						
 <div class="panel panel-default checkout-step-01">
 
-	<!-- panel-heading -->
+	
 		<div class="panel-heading">
     	<h4 class="unicase-checkout-title">
 	        <a data-toggle="collapse" class="" data-parent="#accordion" href="#collapseOne">
@@ -119,11 +109,11 @@ echo "<script>alert('Shipping Address has been updated');</script>";
 	        </a>
 	     </h4>
     </div>
-    <!-- panel-heading -->
+  
 
 	<div id="collapseOne" class="panel-collapse collapse in">
 
-		<!-- panel-body  -->
+		
 	    <div class="panel-body">
 			<div class="row">		
 				<div class="col-md-12 col-sm-12 already-registered-login">
@@ -160,16 +150,16 @@ while($row=mysqli_fetch_array($query))
 					</form>
 					<?php } ?>
 				</div>	
-				<!-- already-registered-login -->		
+	
 
 			</div>			
 		</div>
-		<!-- panel-body  -->
+	
 
-	</div><!-- row -->
+	</div>
 </div>
-<!-- checkout-step-01  -->
-					    <!-- checkout-step-02  -->
+
+					  
 					  	<div class="panel panel-default checkout-step-02">
 						    <div class="panel-heading">
 						      <h4 class="unicase-checkout-title">
