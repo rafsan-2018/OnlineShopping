174efcbf1f62d1e13d2c1876c4b29b3e4d413205
sharif fateh shahriar <68992668+rafsan-2018@users.noreply.github.com>
2020-11-14 22:34:29 +0600

    demo

--- category.php
+++ category.php
@@ -258,4 +258,62 @@ while ($row=mysqli_fetch_array($ret))
 		</div>
 		
 <?php } ?>	
+
+
+
+
+
+		
 	
+
+								  </div>
+							</div>
+						
+						</div>			
+
+				</div>
+			</div>
+		</div></div>
+		<?php include('includes/brands-slider.php');?>
+
+</div>
+</div>
+<?php include('includes/footer.php');?>
+	<script src="assets/js/jquery-1.11.1.min.js"></script>
+	
+	<script src="assets/js/bootstrap.min.js"></script>
+	
+	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	
+	<script src="assets/js/echo.min.js"></script>
+	<script src="assets/js/jquery.easing-1.3.min.js"></script>
+	<script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/jquery.rateit.min.js"></script>
+    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+
+	
+	
+	<script src="switchstylesheet/switchstylesheet.js"></script>
+	
+	<script>
+		$(document).ready(function(){ 
+			$(".changecolor").switchstylesheet( { seperator:"color"} );
+			$('.show-theme-options').click(function(){
+				$(this).parent().toggleClass('open');
+				return false;
+			});
+		});
+
+		$(window).bind("load", function() {
+		   $('.show-theme-options').delay(2000).trigger('click');
+		});
+	</script>
+	
+	
+
+</body>
+</html>
