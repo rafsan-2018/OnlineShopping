50b1929d7eb8116fc02e986aaa8581abecd25102
sharif fateh shahriar <68992668+rafsan-2018@users.noreply.github.com>
2020-11-15 23:10:05 +0600

    session and  Code forProduct deletion from  wishlist

---
+++ my-wishlist.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+    {   
+header('location:login.php');
+}
+else{
+
+$wid=intval($_GET['del']);
+if(isset($_GET['del']))
+{
+$query=mysqli_query($con,"delete from wishlist where id='$wid'");
+}
+
+
+if(isset($_GET['action']) && $_GET['action']=="add"){
+	$id=intval($_GET['id']);
+	$query=mysqli_query($con,"delete from wishlist where productId='$id'");
+	if(isset($_SESSION['cart'][$id])){
+		$_SESSION['cart'][$id]['quantity']++;
+	}else{
+		$sql_p="SELECT * FROM products WHERE id={$id}";
+		$query_p=mysqli_query($con,$sql_p);
+		if(mysqli_num_rows($query_p)!=0){
+			$row_p=mysqli_fetch_array($query_p);
+			$_SESSION['cart'][$row_p['id']]=array("quantity" => 1, "price" => $row_p['productPrice']);	
+header('location:my-wishlist.php');
+}
+		else{
+			$message="Product ID is invalid";
+		}
+	}
+}
+
+?>
